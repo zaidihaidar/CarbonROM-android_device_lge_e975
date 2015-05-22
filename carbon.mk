d56cbf5424a8abbcd17e155baf859984d6310e1e
STELIX <ssspinni@gmail.com>
2015-05-22 22:03:45 +0200

    e975 - STELIXized
    Change-Id: I3b351048254a3649be2e2afec5833c3a93170417
    Signed-off-by: STELIX <ssspinni@gmail.com>

---
+++ carbon.mk
@@ -0,0 +1,39 @@
+# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2014 The Carbon Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 768
+
+# Inherit some common CarbonROM stuff.
+$(call inherit-product, vendor/stelix/config/common_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/e975/e975.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := e975
+PRODUCT_NAME := carbon_e975
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-E975
+PRODUCT_MANUFACTURER := LGE
+CARBON_BUILDTYPE := NIGHTLY
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=geehrc_open_eu \
+                                BUILD_FINGERPRINT=lge/geehrc_open_eu/geehrc:4.1.2/JZO54K/E97510b.1360741472:user/release-keys \
+                                PRIVATE_BUILD_DESC="geehrc_open_eu-user 4.1.2 JZO54K E97510b.1360741472 release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
